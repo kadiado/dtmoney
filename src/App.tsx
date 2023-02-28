3246c3ce1bb3ac81084e4d13c9a91c959186d432
kadiado <marcothulio2@gmail.com>
2023-02-28 00:01:56 -0300

    assets

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import { GlobalStyle } from "./styles/global";
+
 export function App() {
   return (
     <div className="App">
       <h1>Hellooooo</h1>
+      <GlobalStyle />
     </div>
   );
 }
